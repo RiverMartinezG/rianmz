8a98552b7c006d36a3723de45aa00b377d9124bc
River Martínez <rianmartinez@gmail.com>
2021-12-19 21:05:09 -0600

    New version with messages

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Get to know River Martínez`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true
+})
 
 console.log(`
 River Martinez - Mid Wordpress Developer
